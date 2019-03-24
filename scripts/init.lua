ccbfc3ea885abb2fdbf5017d2eaf64447759fd29
duncathan <dunc2403@dunc2403.com>
2019-03-24 17:40:01 -0600

    update init.lua

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,19 +1,9 @@
-Tracker:AddItems("items/common.json")
-Tracker:AddItems("items/dungeon_items.json")
+Tracker:AddItems("items/items.json")
 
 if not (string.find(Tracker.ActiveVariantUID, "items_only")) then
     ScriptHost:LoadScript("scripts/logic_common.lua")
-    ScriptHost:LoadScript("scripts/logic_custom.lua")
-    Tracker:AddMaps("maps/maps.json")
-    if not (string.find(Tracker.ActiveVariantUID, "inverted")) then
-        Tracker:AddLocations("locations/overworld.json")
-    else
-        Tracker:AddLocations("inverted_locations/overworld.json")
-    end
-    Tracker:AddLocations("locations/dungeons.json")    
+    Tracker:AddLocations("locations/locations.json")    
 end
 
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
-
-ScriptHost:LoadScript("scripts/utility_autotracking.lua")
