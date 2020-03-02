7abaad567b5ad2926698c9082626b505e73a64a5
duncathan <dunc@duncathan.com>
2020-03-02 04:42:09 -0600

    adds settings tab to map tracker layout

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,9 +1,11 @@
 Tracker:AddItems("items/items.json")
 
 if not (string.find(Tracker.ActiveVariantUID, "items_only")) then
+    Tracker:AddItems("items/settings.json")
     Tracker:AddMaps("maps/maps.json")
     ScriptHost:LoadScript("scripts/logic_common.lua")
-    Tracker:AddLocations("locations/locations.json")    
+    Tracker:AddLocations("locations/locations.json")
+    Tracker:AddLayouts("layouts/settings.json")
 end
 
 Tracker:AddLayouts("layouts/shared.json")
